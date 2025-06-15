9bd059ccfa73627a1ad9802e1ddafa681b99ec1a
mshklyaeva <maria.shklyaeva0580@gmail.com>
2025-06-15 12:24:15 +0400

    Исправлена ошибка при получении с сервера первого чата

---
+++ WebServer/Handler.cs
@@ -0,0 +1,24 @@
+﻿using MessengerLibrary;
+
+namespace WebServer;
+
+public class Handler
+{
+    public ChatPacket Response {get; set;} 
+    public User Autor {get; set;}
+    public Handler(ChatPacket packet)
+    {
+        if (packet.Message == null)
+        {
+            //this.Response = new ChatPacket();
+        }
+        else
+        {
+            //записать в хранилище
+        }
+    }
+    public Handler(User user)
+    {
+        this.Autor = user;
+    }
+}
--- WebServer/Server.cs
+++ WebServer/Server.cs
@@ -1,18 +1,22 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
+using MessengerLibrary;
 
 namespace WebServer;
 
 class Server
 {
     private TcpListener listener;
+    private InMemoryRepository repository;
 
     public Server(int port)
     {
         listener = new TcpListener(IPAddress.Any, port);
+        repository = new InMemoryRepository();
     }
 
     public void Start()
@@ -34,16 +38,51 @@ class Server
         {
             byte[] buffer = new byte[1024];
             int bytesRead;
+            bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string userMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            var user = User.FromJson(userMessage);
+            if (userMessage != null)
+            {
+                byte[] response;
+                if (!repository.Users.Contains(user))
+                {
+                    Console.WriteLine("пользователя нет");
+                    repository.Users.Add(user);
+                    var generalCart = repository.Chats.Find(x => x.Name == "General");
+                    generalCart.User.Add(user);
+                    Console.WriteLine(GetChatPacket(user).ToJson());
+                    response = Encoding.UTF8.GetBytes(GetChatPacket(user).ToJson());
+                }
+                else
+                {
+                    response = Encoding.UTF8.GetBytes(GetChatPacket(user).ToJson());
+                }
+                stream.Write(response, 0, response.Length);
+            }
 
             while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
             {
-                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                Console.WriteLine("Получено сообщение: " + message);
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead); 
+                //Console.WriteLine("Получено сообщение: " + message);
+                var handler = new Handler(User.FromJson(message));
                 
                 // ответ
-                byte[] response = Encoding.UTF8.GetBytes("Echo: " + message);
-                stream.Write(response, 0, response.Length);
+                //byte[] response = Encoding.UTF8.GetBytes(handler.Response.ToJson());
+                //stream.Write(response, 0, response.Length);
             }
         }
+    }
+
+    private ChatPacket GetChatPacket(User user)
+    {
+        var packet = new ChatPacket();
+        foreach (var chat in repository.Chats)
+        {
+            if (chat.User.Contains(user))
+            {
+                packet.Chats.Add(chat);
+            };
+        }
+        return packet;
     }
 }
